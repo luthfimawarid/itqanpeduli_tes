e3ce973a911ade7f500513330a409f580e8c7665
lutfhiemawarid@gmail.com <lutfhiemawarid@gmail.com>
2024-07-04 08:09:35 +0700

    sidebar

---
+++ itqan_peduli/resources/views/admin/isi/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.utama.index')
+@section('content')
+    
+@endsection
---
+++ itqan_peduli/resources/views/admin/utama/sidebar.blade.php
@@ -0,0 +1,67 @@
+<div class="bungkus mx-3 mt-4 font-semibold">
+    <div class="w-64 h-screen bg-white rounded-lg shadow-2xl">
+        <div class="p-6">
+            <h1 class="text-2xl font-bold">Logo</h1>
+        </div>
+        <nav class="mt-10 text-sm">
+            <a href="#" class="flex flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                <svg class="w-5 h-5 my-auto me-2 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path d="M5 3a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5Zm14 18a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h4ZM5 11a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2H5Zm14 2a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h4Z"/>
+                </svg>
+                Dashboard
+            </a>
+            <hr class="mx-auto rounded-lg my-2" style="width: 80%;">
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                <svg class="w-5 h-5 my-auto me-2 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12 3a1 1 0 0 1 .78.375l4 5a1 1 0 1 1-1.56 1.25L13 6.85V14a1 1 0 1 1-2 0V6.85L8.78 9.626a1 1 0 1 1-1.56-1.25l4-5A1 1 0 0 1 12 3ZM9 14v-1H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-4v1a3 3 0 1 1-6 0Zm8 2a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clip-rule="evenodd"/>
+                </svg>
+                Publikasi Program
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                <svg class="w-5 h-5 my-auto me-2 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/>
+                </svg>
+                Pengaturan Program
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                Transaksi
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                Dana Terkumpul
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                Penyaluran Dana
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                Dana Donatur
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                Fundraiser
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+            <a href="#" class="flex block py-3.5 px-4 rounded transition duration-200 hover:bg-green-200 hover:text-black">
+                Web Utama
+                <svg class="w-3 h-3 my-auto ms-auto text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
+                </svg>
+            </a>
+        </nav>
+    </div>
+</div>
